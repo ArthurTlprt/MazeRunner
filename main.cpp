0597b2eb343c9dfb0f669ddbe8b69bb7100c1fb0
Guillaume Courmont <guillaume.courmont@isen-lille.fr>
2016-04-05 11:17:08 +0200

    Main update for an exemple of how to use class cell.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
-
+#include "cell.h"
+#include <iostream>
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	cell firstCell(1);
+    cell secondCell(2);
+    firstCell.setRightCell(&secondCell);
+    cell* ptr = firstCell.getNextCell();
+    std::cout << firstCell.getId() << std::endl;
+    std::cout << ptr->getId() << std::endl;
+
+    /**/
 	return 0;
 }
