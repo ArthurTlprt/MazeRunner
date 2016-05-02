0c4a32a4c2bcbfc1a81579fc4490e2bed1b8e19a
ArthurT <arthur.talpaert@isen-lille.fr>
2016-05-02 19:54:20 +0200

    ajout fichiers

---
+++ generator/maze.h
@@ -0,0 +1,50 @@
+/*
+ * File:   maze.h
+ * Author: isen
+ *
+ * Created on 7 avril 2016, 11:07
+ */
+
+#ifndef MAZE_H
+#define	MAZE_H
+
+#include "cell.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <ctime>
+#include <unordered_set>
+#include <chrono>
+#include <thread>
+#include "json.hpp"
+using json = nlohmann::json;
+
+
+
+class maze {
+public:
+    maze();
+    maze(int width, int height);
+    maze(const maze& orig);
+    virtual ~maze();
+    void print();
+    void debug();
+    json save();
+    cell getCell(int x, int y);
+    void generate(cell&);
+private:
+    std::vector<std::vector<cell> > cells;
+    std::stack<cell*> antecedent;
+    std::unordered_set<int> visited;
+    void clear();
+    void initCells();
+    void printHori(int d);
+    void printVert(int d);
+    std::vector<int> getNexts(cell&);
+    int width;
+    int height;
+};
+
+#endif	/* MAZE_H */
