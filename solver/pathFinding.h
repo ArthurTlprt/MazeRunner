cd84d17d915c41a5bdba72056d431c7ad56d11ef
Guillaume Courmont <guillaume.courmont@isen-lille.fr>
2016-05-02 20:05:56 +0000

    pathFinding.h édité en ligne via Bitbucket

--- solver/pathFinding.h
+++ solver/pathFinding.h
@@ -3,9 +3,12 @@
 #include <string>
 #include <list>
 #include <map>
+#include "../generator/json.hpp"
+using json = nlohmann::json;
 
 std::vector<node> createSuccessor(std::string successorsString);
 float distance(node goal, node current);
 std::multimap<int,int> parseSuccessor(std::string successor);
 void displayList(std::list<node> list, std::string name);
 int findPath(std::map<std::string,std::string> maze_map,int size);
+std::map<std::string,std::string> recup(json maze);
